279c2ff54e5cab05e0f124580a7131de12af328d
nyamennaefrederick-art <nyamennaefrederick@gmail.com>
2026-07-18 17:03:29 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -5,31 +5,30 @@ let phone = document.getElementById("phone").value;
 let network = document.getElementById("network").value;
 
 
-if(name.trim() === ""){
+if(name.trim()===""){
 alert("Please enter your name");
 return;
 }
 
-if(phone.trim() === ""){
+if(phone.trim()===""){
 alert("Please enter your phone number");
 return;
 }
 
 
 let message =
-"Hello CND Instant Data Bundle\n\n" +
-"Name: " + name +
-"\nPhone: " + phone +
-"\nNetwork: " + network +
-"\nPackage: " + data +
-"\nPrice: GHS " + price;
+"Hello CND Instant Data Bundle\n\n"+
+"Name: "+name+
+"\nPhone: "+phone+
+"\nNetwork: "+network+
+"\nPackage: "+data+
+"\nPrice: GHS "+price;
 
 
 let whatsapp =
-"https://wa.me/233543553686?text=" 
-+ encodeURIComponent(message);
+"https://wa.me/233543553686?text="+encodeURIComponent(message);
 
 
-window.open(whatsapp, "_blank");
+window.open(whatsapp,"_blank");
 
 }
