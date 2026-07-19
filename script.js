14e6f6ca23884b167fe165ae5c9de879630ba362
nyamennaefrederick-art <nyamennaefrederick@gmail.com>
2026-07-19 14:21:59 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-function buyData(data, price){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpD5ShNaSByzGPxF1FlNGS0zgRZPAtnDg",
+  authDomain: "cnd-data-bundle.firebaseapp.com",
+  projectId: "cnd-data-bundle",
+  storageBucket: "cnd-data-bundle.firebasestorage.app",
+  messagingSenderId: "667849737543",
+  appId: "1:667849737543:web:a8571f7123483b4765f862"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+window.buyData = async function(data, price){
 
 let name = document.getElementById("name").value;
 let phone = document.getElementById("phone").value;
 let network = document.getElementById("network").value;
+
 
 if(name === "" || phone === ""){
 alert("Please enter your name and phone number");
 return;
 }
 
-let order = {
-name: name,
-phone: phone,
-network: network,
-package: data,
-price: price,
-status: "Pending"
-};
 
-// Save order using phone number
-localStorage.setItem(phone, JSON.stringify(order));
+try{
+
+await addDoc(collection(db,"orders"),{
+
+name:name,
+phone:phone,
+network:network,
+package:data,
+price:price,
+status:"Pending",
+date:new Date()
+
+});
+
+
+alert("Order received successfully");
 
 
 let message =
@@ -30,11 +53,18 @@ let message =
 "\nPackage: "+data+
 "\nPrice: GHS "+price;
 
-let url =
-"https://wa.me/233543553686?text="+encodeURIComponent(message);
 
-alert("Order placed successfully!");
+window.open(
+"https://wa.me/233543553686?text="+encodeURIComponent(message),
+"_blank"
+);
 
-window.open(url,"_blank");
+
+}catch(error){
+
+console.log(error);
+alert("Order failed. Try again.");
+
+}
 
 }
